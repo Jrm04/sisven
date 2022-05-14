8b892ef780cd4e37e76d75cf80cd49fa8d80c4ea
Jehiko Mosquera <jeramos04@hotmail.com>
2022-05-13 23:53:49 -0500

    Commit inicial

---
+++ database/migrations/2022_05_06_010336_create_invoce_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invoce', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('client_id');
+            $table->unsignedBigInteger('num_pago');
+            $table->text('other_details');
+            $table -> foreign('client_id')->references('id')->on('client');
+            $table -> foreign('num_pago')->references('id')->on('pay_method');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('invoce');
+    }
+};
---
+++ database/migrations/2022_05_09_232956_create_detail_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('detail', function (Blueprint $table) {
+            $table->unsignedBigInteger('id_detail');
+            $table->unsignedBigInteger('id_invoce');
+            $table->primary(['id_detail', 'id_invoce']);  //$table->id('num_detalle', 'id_invoce');
+            $table->unsignedBigInteger('id_product');
+            $table->string('amount', 5);
+            $table->decimal('price', 8,3);
+            $table->foreign('id_invoce')->references('id')->on('invoce');
+            $table->foreign('id_product')->references('id')->on('product');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('detail');
+    }
+};
